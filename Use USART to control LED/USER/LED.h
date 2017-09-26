84d7ec077c8257b328c9ad362eecc57969f2fec0
YiQun Chen <c872534679@qq.com>
2017-09-26 21:15:08 +0800

    Use USART to control LED

--- Use USART to control LED/USER/LED.h
+++ Use USART to control LED/USER/LED.h
@@ -3,16 +3,14 @@
 #include "stm32f10x.h"
 #define ON 0
 #define OFF 1
-#define LED_Init_Port GPIOB
-#define LED_Init_Pin  GPIO_Pin_10
-#define LED_Init_CLK  RCC_APB2Periph_GPIOB
+#define LED_Init_Port GPIOE
+#define LED_Init_Pin  GPIO_Pin_6
+#define LED_Init_CLK  RCC_APB2Periph_GPIOE
 #define LED(a) if(a)\
 	GPIO_SetBits(LED_Init_Port,LED_Init_Pin);\
 else\
 	GPIO_ResetBits(LED_Init_Port,LED_Init_Pin);
 void LED_GPIO_Config(void);
-#define LED_TOGGLE LED_Init_Port->ODR^=LED_Init_Pin
-
 
 #endif //__LED_H
 
